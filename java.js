44fa58dbe8fa31354a7bc0ae217ac470bcc0deb5
JustJaxttv <156222998+JustJaxttv@users.noreply.github.com>
2024-02-11 20:26:18 -0600

    2/11

---
+++ java.js
@@ -0,0 +1,10 @@
+document.getElementById("chiefs").addEventListener("click", chiefsWin)
+document.getElementById("49rs").addEventListener("click", ninersWin)
+
+function chiefsWin() {
+    document.getElementById("chiefsDiv").backgroundColor = "yellow";
+}
+
+function ninersWin() {
+    document.getElementById("ninersDiv")
+}
