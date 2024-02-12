2a3c22dc52b316f27b07c7863a7dec8c8a6630e1
JustJaxttv <156222998+JustJaxttv@users.noreply.github.com>
2024-02-11 21:58:31 -0600

    2/11

--- java.js
+++ java.js
@@ -1,10 +1,30 @@
-document.getElementById("chiefs").addEventListener("click", chiefsWin)
-document.getElementById("49rs").addEventListener("click", ninersWin)
+document.getElementById("chiefs").addEventListener("click", chiefsWin);
+document.getElementById("49rs").addEventListener("click", ninersWin);
 
 function chiefsWin() {
-    document.getElementById("chiefsDiv").backgroundColor = "yellow";
+    document.getElementById("chiefsDiv").style.backgroundColor = "yellow";
+    document.getElementById("ninersDiv").style.backgroundColor = "#B3995D";
 }
 
 function ninersWin() {
-    document.getElementById("ninersDiv")
+    document.getElementById("ninersDiv").style.backgroundColor = "yellow";
+    document.getElementById("chiefsDiv").style.backgroundColor = "#B3995D";
+}
+
+function favTeam() {
+    let x = document.getElementById("team").value;
+    document.getElementById("fav").innerHTML = x;
+}
+
+function revealWinner() {
+
+    i = 0;
+    while (i <= 4) {
+        var newParagraph = document.createElement("p");
+        var text = document.createTextNode("The Chiefs won the Super Bowl!");
+        newParagraph.appendChild(text);
+        var element = document.getElementById("winner")
+        element.appendChild(newParagraph);
+        i = i + 1;
+    }
 }
